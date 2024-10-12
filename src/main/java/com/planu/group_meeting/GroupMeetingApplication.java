4924db3aaa4bcba352d2f84c051be3731a2c61e7
sangjun <sangjoon12345@gmail.com>
2024-10-12 14:18:45 +0900

    Initial project setup

---
+++ src/main/java/com/planu/group_meeting/GroupMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.planu.group_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupMeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planu/group_meeting/GroupMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planu.group_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
