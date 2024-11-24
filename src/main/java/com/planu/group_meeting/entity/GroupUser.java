aca5b48a794b158d309318e9cb73d9a952d3a1d0
ehgk4245 <ehgkrk14@naver.com>
2024-11-24 22:07:24 +0900

    feat: 그룹 생성(채팅방 생성) 기능 추가
    - 그룹 명과 그룹 이미지 의 형식 및 크기 제한, 검증 클래스에서 검증 후 검증 실패시 에러 메시지 전달
    - groupService, GroupDAO, GroupMapper 를 사용해 GROUP_ 테이블과 GROUP_USER 테이블에 삽입

---
+++ src/main/java/com/planu/group_meeting/entity/GroupUser.java
@@ -0,0 +1,42 @@
+package com.planu.group_meeting.entity;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GroupUser {
+
+    private Long userId;
+    private Long groupId;
+    private GroupRole groupRole;  // Enum을 사용
+    private Integer groupState;  // 상태를 나타내는 Integer로 매핑
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @Getter
+    public enum GroupRole {
+        LEADER("LEADER"),
+        PARTICIPANT("PARTICIPANT");
+
+        private String role;
+
+        GroupRole(String role) {
+            this.role = role;
+        }
+
+        public static GroupRole fromString(String role) {
+            for (GroupRole groupRole : GroupRole.values()) {
+                if (groupRole.getRole().equals(role)) {
+                    return groupRole;
+                }
+            }
+            return PARTICIPANT; // 기본값 설정
+        }
+    }
+}
+
+
