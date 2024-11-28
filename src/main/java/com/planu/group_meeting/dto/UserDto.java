a33106ed7267f9a34627ef27497dcb81e580a1a1
sangjun <sangjoon12345@gmail.com>
2024-11-28 22:18:36 +0900

    feat: 회원가입 유효성 검사 구현

--- src/main/java/com/planu/group_meeting/dto/UserDto.java
+++ src/main/java/com/planu/group_meeting/dto/UserDto.java
@@ -3,9 +3,7 @@ package com.planu.group_meeting.dto;
 import com.planu.group_meeting.entity.common.Gender;
 import com.planu.group_meeting.entity.common.Role;
 import com.planu.group_meeting.entity.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,17 +16,20 @@ public class UserDto {
     @Setter
     @NoArgsConstructor
     public static class SignUpRequest {
-        @NotBlank(message = "아이디를 입력해주세요!")
+        @NotBlank(message = "아이디를 입력해주세요")
+        @Pattern(regexp = "^[a-z0-9]{5,12}$", message = "아이디는 5~12자의 영어 소문자와 숫자만 입력 가능합니다.")
         private String username;
 
-        @NotBlank(message = "비밀번호를 입력해주세요!")
+        @NotBlank(message = "비밀번호를 입력해주세요")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String password;
 
-        @NotBlank(message = "이름을 입력해주세요!")
+        @NotBlank(message = "이름을 입력해주세요")
+        @Pattern(regexp = "^[가-힣]{2,6}$", message = "이름은 2자 이상 6자 이하의 한글만 입력 가능합니다.")
         private String name;
 
         @NotBlank(message = "이메일 주소를 입력해주세요!")
-        @Email(message = "올바른 이메일 주소를 입력해주세요!")
+        @Email(message = "올바른 이메일 주소를 입력해주세요")
         private String email;
 
         public User toEntity() {
