f14722e95fe41fc65e8674af547741b915f26806
sangjun <sangjoon12345@gmail.com>
2025-01-27 17:44:58 +0900

    refactor: 유저 식별자 프론트에게 전송할때 username으로 변경

--- src/main/java/com/planu/group_meeting/dto/FriendDto.java
+++ src/main/java/com/planu/group_meeting/dto/FriendDto.java
@@ -1,5 +1,6 @@
 package com.planu.group_meeting.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,6 +20,7 @@ public class FriendDto {
     @Getter
     @Setter
     public static class FriendInfo{
+        @JsonIgnore
         private Long userId;
         private String name;
         private String username;
