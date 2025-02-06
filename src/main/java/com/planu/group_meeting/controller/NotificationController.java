e02dbb9bd2a70324e09a8140d7efa43963a8970d
sangjun <sangjoon12345@gmail.com>
2025-02-07 00:18:22 +0900

    feat: 알림 목록 조회 기능 구현

--- src/main/java/com/planu/group_meeting/controller/NotificationController.java
+++ src/main/java/com/planu/group_meeting/controller/NotificationController.java
@@ -1,7 +1,11 @@
 package com.planu.group_meeting.controller;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.planu.group_meeting.config.auth.CustomUserDetails;
+import com.planu.group_meeting.dto.BaseResponse;
+import com.planu.group_meeting.dto.NotificationDTO;
 import com.planu.group_meeting.service.NotificationService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notification")
@@ -21,6 +27,11 @@ public class NotificationController {
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter>subscribeEmitter(@AuthenticationPrincipal CustomUserDetails userDetails){
         return ResponseEntity.ok(notificationService.createEmitter(userDetails.getId()));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<NotificationDTO>> getNotificationList(@AuthenticationPrincipal CustomUserDetails userDetails){
+        return ResponseEntity.ok(notificationService.getNotificationList(userDetails.getId()));
     }
 
 }
