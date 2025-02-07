6e33eea6f296587ceee12f84c3616349b92357ce
sj0320 <112684207+sj0320@users.noreply.github.com>
2025-02-07 14:48:07 +0900

    Update GroupMeetingApplication.java

--- src/main/java/com/planu/group_meeting/GroupMeetingApplication.java
+++ src/main/java/com/planu/group_meeting/GroupMeetingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan("com.planu.group_meeting.dao")
 public class GroupMeetingApplication {
 
