4f447aa4aca67cce310ed760b66a0960b57f44ec
belight2 <chlwnsgur090@gmail.com>
2025-02-07 18:46:46 +0900

    fix: 그룹 일정 상세 조회 수정

--- src/main/java/com/planu/group_meeting/GroupMeetingApplication.java
+++ src/main/java/com/planu/group_meeting/GroupMeetingApplication.java
@@ -3,6 +3,7 @@ package com.planu.group_meeting;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
