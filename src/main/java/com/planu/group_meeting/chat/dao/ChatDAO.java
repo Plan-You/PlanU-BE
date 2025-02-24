44d65db17e606bbeef6809d5ab846ef9f104dcce
ehgk4245 <ehgkrk14@naver.com>
2025-02-24 14:46:02 +0900

    feat: 채팅(진행중)

--- src/main/java/com/planu/group_meeting/chat/dao/ChatDAO.java
+++ src/main/java/com/planu/group_meeting/chat/dao/ChatDAO.java
@@ -1,9 +1,10 @@
 package com.planu.group_meeting.chat.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ChatDAO {
 
-//    public Long save(@Param("userId") Long userId, @Param("groupId") Long groupId, @Param("content") String content);
+    public Long save(@Param("userId") Long userId, @Param("groupId") Long groupId, @Param("content") String content);
 }
