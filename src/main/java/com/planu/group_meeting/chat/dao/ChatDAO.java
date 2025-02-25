03505c5dcba48fd36afb1a55997d2bd5ec375236
ehgk4245 <ehgkrk14@naver.com>
2025-02-25 20:06:23 +0900

    feat: 채팅방 목록 조회 완료

--- src/main/java/com/planu/group_meeting/chat/dao/ChatDAO.java
+++ src/main/java/com/planu/group_meeting/chat/dao/ChatDAO.java
@@ -1,5 +1,6 @@
 package com.planu.group_meeting.chat.dao;
 
+import com.planu.group_meeting.chat.dto.ChatInfo;
 import com.planu.group_meeting.chat.dto.ChatMessage;
 import com.planu.group_meeting.chat.dto.MessageStatus;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,6 +10,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ChatDAO {
 
     void saveChatMessage(ChatMessage chatMessage);
-
     void saveMessageStatus(MessageStatus messageStatus);
+
+    ChatInfo getChatInfo(Long groupId);
 }
