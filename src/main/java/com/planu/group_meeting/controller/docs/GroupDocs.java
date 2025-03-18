9d161edf0b929f68298abbd31b18d21a77f76b8e
ehgk4245 <ehgkrk14@naver.com>
2025-03-18 19:00:26 +0900

    feat : 채팅 및 그룹 api 스웨거 설정

--- src/main/java/com/planu/group_meeting/controller/docs/GroupDocs.java
+++ src/main/java/com/planu/group_meeting/controller/docs/GroupDocs.java
@@ -118,32 +118,284 @@ public interface GroupDocs {
                                                       @RequestParam("groupId") Long id,
                                                       @RequestParam("username") String userName);
 
+    @Operation(summary = "그룹 초대 취소", description = "그룹 리더가 보낸 초대를 취소합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "초대 취소 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"초대 취소 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "초대 거절 권한이 없을 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"초대 권한이 없습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 그룹이 존재하지 않을 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"해당 그룹이 존재하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "이미 취소 되었거나 유저가 없는경우",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"사용자 '\" + userName + \"'을 찾을 수 없습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 유저가 이미 그룹초대를 수락한 경우",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"사용자 '\" + username + \"'은 이미 그룹의 멤버입니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> inviteUserCancel(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                   @RequestParam("groupId") Long groupId,
                                                   @RequestParam("username") String username);
 
+    @Operation(summary = "초대 수락", description = "그룹 초대를 수락 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "초대 수락 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"초대 수락 하였습니다.\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "그룹을 찾을 수 없는 경우.",
+                                            value = "{\n" +
+                                                    "  \"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"해당 그룹이 존재하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "초대 받지 않은경우.",
+                                            value = "{\n" +
+                                                    "  \"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"초대 받지 않았습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "이미 그룹에 있는 경우.",
+                                            value = "{\n" +
+                                                    "  \"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"이미 그룹에 속해 있습니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> joinGroup(@AuthenticationPrincipal CustomUserDetails userDetails,
                                            @PathVariable("groupId") Long groupId);
 
+    @Operation(summary = "그룹 조회", description = "사용자가 속한 그룹 목록을 보여줍니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "그룹 조회 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "[\n" +
+                                    "  \"data\" : [\n" +
+                                    "\t  {\n" +
+                                    "\t    \"groupId\": 1,\n" +
+                                    "\t    \"groupName\": \"Study Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group1.png\",\n" +
+                                    "\t    \"participant\": 10,\n" +
+                                    "\t    \"isPin\" : true\n" +
+                                    "\t  },\n" +
+                                    "\t  {\n" +
+                                    "\t    \"groupId\": 2,\n" +
+                                    "\t    \"groupName\": \"Workout Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group2.png\",\n" +
+                                    "\t    \"participant\": 15,\n" +
+                                    "\t    \"isPin\" : true\n" +
+                                    "\t  },\n" +
+                                    "\t  {\n" +
+                                    "\t    \"groupId\": 3,\n" +
+                                    "\t    \"groupName\": \"Gaming Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group3.png\",\n" +
+                                    "\t    \"participant\": 8,\n" +
+                                    "\t    \"isPin\" : false\n" +
+                                    "\t\t}\n" +
+                                    "  ]\n" +
+                                    "]")))
+    })
     ResponseEntity<DataResponse<List<GroupResponseDTO>>> groupList(
             @AuthenticationPrincipal CustomUserDetails userDetails);
 
+    @Operation(summary = "초대 조회", description = "초대 받은 목록을 보여줍니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "초대 조회 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"data\" : [\n" +
+                                    "\t\t{\n" +
+                                    "\t    \"groupId\": 1,\n" +
+                                    "\t    \"groupName\": \"Study Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group1.png\",\n" +
+                                    "\t  },\n" +
+                                    "\t  {\n" +
+                                    "\t    \"groupId\": 2,\n" +
+                                    "\t    \"groupName\": \"Workout Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group2.png\",\n" +
+                                    "\t  },\n" +
+                                    "\t  {\n" +
+                                    "\t    \"groupId\": 3,\n" +
+                                    "\t    \"groupName\": \"Gaming Group\",\n" +
+                                    "\t    \"groupImageUrl\": \"https://example.com/group3.png\",\n" +
+                                    "\t\t}\n" +
+                                    "\t]\n" +
+                                    "}")))
+    })
     ResponseEntity<DataResponse<List<GroupResponseDTO>>> groupInviteList(
             @AuthenticationPrincipal CustomUserDetails userDetails);
 
+    @Operation(summary = "그룹 탈퇴", description = "해당 그룹에서 탈퇴합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "그룹 탈퇴 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"그룹 탈퇴 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "해당 그룹에서 유저를 찾을 수 없는 경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"이미 그룹에 속하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "그룹 초대메시지를 수락/거절 하지 않은 경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"초대 수락/거절을 먼저 수행하십시오.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 그룹에서 리더인 경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"그룹 리더는 그룹을 떠날 수 없습니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> leaveGroup(@AuthenticationPrincipal CustomUserDetails userDetails,
                                             @PathVariable("groupId") Long groupId);
 
+    @Operation(summary = "그룹 삭제", description = "해당 그룹을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "그룹 삭제 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"그룹 삭제 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "해당 그룹이 이미 없는 경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"해당 그룹이 존재하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 그룹의 리더가 아닌경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"그룹 삭제 권한이 없습니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> deleteGroup(@AuthenticationPrincipal CustomUserDetails userDetails,
                                              @PathVariable("groupId") Long groupId);
 
+    @Operation(summary = "초대 거절", description = "초대를 거절합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "초대 거절 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"초대 거절 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "그룹을 찾을 수 없는 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"해당 그룹이 존재하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "초대 받지 않은경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"초대 받지 않았습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "이미 그룹에 있는 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"이미 그룹에 속해 있습니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> declineInvitation(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                    @PathVariable("groupId") Long groupId);
 
+    @Operation(summary = "강제 탈퇴", description = "그룹리더가 그룹원을 강제 탈퇴 시킵니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "강제 탈퇴 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"강제 퇴출 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "해당 그룹의 리더가 아닌 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"강제 퇴출시킬 권한이 없습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 그룹에 속해 있지 않은멤버를 퇴출시키는 경우",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"그룹에 속해 있지 않은 멤버입니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "리더가 자신을 퇴출시키려 하는 경우.",
+                                            value = "{\n" +
+                                                    "  \"ResultCode\": 400,\n" +
+                                                    "  \"ResultMsg\": \"자기자신을 퇴출시킬 수 없습니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> forceExpelMember(@AuthenticationPrincipal CustomUserDetails userDetails,
                                                   @PathVariable("groupId") Long groupId,
                                                   @PathVariable("username") String username);
 
+    @Operation(summary = "그룹 상단 고정/해제", description = "해당 그룹을 상단 고정/해제 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "상단 고정/해제 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = "{\n" +
+                                    "\t\"resultCode\": 200,\n" +
+                                    "  \"resultMsg\": \"상단 고정/해제 성공\"\n" +
+                                    "}"))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(name = "해당 그룹이 없는 경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"해당 그룹이 존재하지 않습니다.\"\n" +
+                                                    "}"),
+                                    @ExampleObject(name = "해당 그룹의 그룹원이 아닌경우",
+                                            value = "{\n" +
+                                                    "\t\"resultCode\": 400,\n" +
+                                                    "  \"resultMsg\": \"해당 그룹의 그룹원이 아닙니다.\"\n" +
+                                                    "}")
+                            })
+            )
+    })
     ResponseEntity<BaseResponse> pinedGroup(@AuthenticationPrincipal CustomUserDetails userDetails,
                                             @PathVariable("groupId") Long groupId);
 
